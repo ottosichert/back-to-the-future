e5071578f49448f0bfde127fce961afcc826b5e0
Otto Sichert <git@ottosichert.de>
2019-10-16 12:38:12 +0100

    Use names for slides instead of numbers

--- src/presentation.js
+++ src/presentation.js
@@ -3,15 +3,15 @@ import React from 'react';
 
 import { Deck } from './components';
 import './quine.css';
-import Slide1 from './slides/Slide1';
-import Slide2 from './slides/Slide2';
-import Slide3 from './slides/Slide3';
+import Cover from './slides/Cover';
+import Theme from './slides/Theme';
+import Quine from './slides/Quine';
 import theme from './theme';
 
 export default () => (
   <Deck progress="bar" theme={theme}>
-    <Slide1 />
-    <Slide2 />
-    <Slide3 />
+    <Cover />
+    <Theme />
+    <Quine />
   </Deck>
 );
