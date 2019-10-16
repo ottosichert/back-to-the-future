b19a9f5a7e18d318596e84391e6e254d83694fe1
Otto Sichert <git@ottosichert.de>
2019-10-16 02:44:38 +0100

    Move slides to separate folder

--- src/presentation.js
+++ src/presentation.js
@@ -1,37 +1,12 @@
 import 'normalize.css';
 import React from 'react';
 
-import {
-  Center,
-  Deck,
-  Fill,
-  Heading,
-  Image,
-  Layout,
-  Link,
-  Slide,
-  Text,
-} from './components';
+import { Deck } from './components';
+import Slide1 from './slides/Slide1';
 import theme from './theme';
 
 export default () => (
   <Deck progress="bar" theme={theme}>
-    <Slide>
-      <Layout>
-        <Center>
-          <Heading fit>Back to the Future</Heading>
-          <Text fit>Modern CSS without Tech Debt</Text>
-        </Center>
-        <Fill>
-          <Image src="https://github.com/ottosichert.png" width={200} />
-          <Text textSize="1.5rem" margin="2rem 0 0" bold>Otto Sichert</Text>
-          <Text textSize="1.5rem">Software Development Engineer</Text>
-          <Link textSize="1.5rem" href="https://aws.amazon.com/rds" />
-          <Link textSize="1.5rem" margin="2rem 0 0" href="mailto:git@ottosichert.de" />
-          <Link textSize="1.5rem" href="https://github.com/ottosichert" />
-          <Link textSize="1.5rem" href="https://linkedin.com/in/ottosichert" />
-        </Fill>
-      </Layout>
-    </Slide>
+    <Slide1 />
   </Deck>
 );
